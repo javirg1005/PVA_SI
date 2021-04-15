535c30e458a5ca5b1afec4ad13190df96d9b60d5
javirg1005 <55756129+javirg1005@users.noreply.github.com>
2021-04-15 19:50:13 +0200

    Update Audio.py
    Co-Authored-By: Caco <55552731+cacolyoko@users.noreply.github.com>

--- Audio.py
+++ Audio.py
@@ -3,28 +3,31 @@ import speech_recognition as sr
 import os
 import eel 
 from moviepy.video.io.ffmpeg_tools import ffmpeg_extract_subclip
+from moviepy.video.io.VideoFileClip import VideoFileClip
 
+'''
 eel.init('web')
 eel.start('index.html')
+'''
 
-
-def main(path):
-    trocear_video(path)
+def chufla(path):
+    texto = trocear_videoV2(path)
     print("video troceado")
-    texto = ""
     '''
-    while True: ##Trabajando en como hacer el bucle y la i
-        video = mp.VideoFileClip(".\\SVideo\\" + i +".mp4")
-        tratamiento_video(video, i)
-        texto = texto + tratamiento_video
-    #se edita
+    texto = ""
+    
+    onlyfiles = next(os.walk("\\SVideo"))[2] #dir is your directory path as string
+    n_files = len(onlyfiles)
+    
+    for i in range(n_files):
+        video = mp.VideoFileClip(".\\SVideo\\" + i +".mp4")      
+        texto = texto + tratamiento_video(video)
+    #se edita'''
     
     #se etiqueta
 
     #se imprime en un txt
     escribir_transcripcion(texto)
-'''
-
 
 
 
@@ -42,21 +45,36 @@ def trocear_video(path):
         i = i + 1
         chunk_dur_ini = chunk_dur_ini + 60
         chunk_dur_fin = chunk_dur_fin + 60
+
+def trocear_videoV2(path):
+    required_video_file = mp.VideoFileClip(path)
+    chunk_dur_ini = 0
+    chunk_dur_fin = 60
+    texto = ''
+    duracion = int(required_video_file.duration) #Duracion del video
+    while (duracion > chunk_dur_ini):
+        starttime = chunk_dur_ini
+        endtime = chunk_dur_fin
+        with VideoFileClip(path) as video:
+            #new = video.subclip(starttime, endtime)
+            new = ffmpeg_extract_subclip(path, starttime, endtime, targetname = "D:\Github\PVA_SI\PruebasSplit\Meh.mp4")
+            texto = texto + tratamiento_video(new)
+        chunk_dur_ini = chunk_dur_ini + 60
+        chunk_dur_fin = chunk_dur_fin + 60
+    return texto
     
-def tratamiento_video(video, i):
-    audioVideo = video.audio.write_audiofile(i+'.wav') #Se escribe extra el codigo ha un archivo
-    #Se reduce el ruido
+def tratamiento_video(video):
+    audio= video.audio.write_audiofile(r'AudioV.wav')
     recibido_cambio = sr.Recognizer()
     speech_audio = sr.AudioFile('AudioV.wav')
-    speech_audio = sr.AudioFile(audioVideo)
-
-    speech_audio = speech_audio
 
     with speech_audio as fuente:
+        #Se reduce el ruido
         recibido_cambio.adjust_for_ambient_noise(fuente)
         audio = recibido_cambio.record(fuente)
         #Transcripcion
         texto_transcrito = recibido_cambio.recognize_google(audio, language = "es-ES")
+        print(texto_transcrito)
         return texto_transcrito
 
 def escribir_transcripcion(lista):
@@ -66,6 +84,7 @@ def escribir_transcripcion(lista):
     archivo_transcrito.close()
     print ("Se ha guardado la transcripción en el archivo " + nombre_archivo_transcrito + " en tu ruta actual") 
 
+chufla('PruebasSplit\VideoTest.mp4')
 #Reconocimiento
     
 #Edicion
